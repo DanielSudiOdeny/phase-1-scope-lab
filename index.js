61163b4b097d0f6fdca77d9ca3ad7d60c6aebdb0
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-06-15 14:06:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(customerName) {
+  bestCustomer = customerName;
+}
+
+const leastFavoriteCustomer = "Daniel";
+
+function changeLeastFavoriteCustomer() {
+  return (leastFavoriteCustomer = "Bob");
+}
